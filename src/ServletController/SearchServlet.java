a3b77250d544128a79b50b41012bc1c7bc48a872
磊子狗 <@leizigou>
2015-07-20 13:54:16 +0800

    7.20

--- src/ServletController/SearchServlet.java
+++ src/ServletController/SearchServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Dao.Room;
 import DataBase.DataService;
 import DataBase.DataServiceimpl;
 
@@ -23,8 +24,19 @@ public class SearchServlet extends HttpServlet {
 			if(one.indexOf(detail)!=-1)
 				wantRoom.add(one);
 		}
+		ArrayList<Room> list = new ArrayList<Room>();
+		for(String one:wantRoom){
+			String roomID = one.split("//")[0];
+			String roomName = one.split("//")[1];
+			String number = one.split("//")[2];
+			if(roomID.equals("-1"))
+				continue;
+			Room room = new Room(roomID, roomName);
+			room.setPnum(Integer.valueOf(number));
+			list.add(room);
+		}
 		request.setAttribute("list", 111);
-		request.setAttribute("wantroom", wantRoom);
+		request.setAttribute("wantroom", list);
 		request.getRequestDispatcher("/page.jsp").forward(request, response);
 	}
 
